ae70992e98ba0a146b55804c6a8d05599443fd5b
Siarhei.Sauras <sergei.sovras@gmail.com>
2022-04-11 19:30:18 +0300

    Part_1_Page_Object etree

---
+++ Secret_sauce.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from lxml import etree
+
+
+browser = webdriver.Chrome()
+browser.get("https://techstepacademy.com/trial-of-the-stones")
+divs = browser.find_elements_by_xpath("//div/span/..")
+tree = etree.HTML(browser.page_source)
+tree.findall(".//div/span/..")
