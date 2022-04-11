8be5c7c83db7bc434ecd42409a458451c24bacca
Siarhei.Sauras <sergei.sovras@gmail.com>
2022-04-11 21:09:57 +0300

    Part_1_Page_Object_final

--- Secret_sauce.py
+++ Secret_sauce.py
@@ -7,3 +7,9 @@ browser.get("https://techstepacademy.com/trial-of-the-stones")
 divs = browser.find_elements_by_xpath("//div/span/..")
 tree = etree.HTML(browser.page_source)
 tree.findall(".//div/span/..")
+merchant_divs = tree.findall(".//div/span/..")
+merchant_divs
+first_merchant = merchant_divs[0]
+first_merchant
+first_merchant.find("./span/b")
+first_merchant.find("./span/b").text
